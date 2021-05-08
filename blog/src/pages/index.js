2d3a88b90b2bf361db84af957569e45e7db826c8
steel <task4kj@gmail.com>
2021-05-08 10:46:18 +0900

    blog fileの追加

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,12 +1,42 @@
+// src/pages/index.js
 import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
+import PostLink from "../components/post-link"
+import { graphql } from "gatsby"
+import SEO from "../components/seo"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
+      <SEO title="Dev Blog" description="Gatsbyを使って作ったブログです" />
       <Hero />
+      {data.allContentfulPost.edges.map(edge =>
+        <PostLink key={edge.node.slug} post={edge.node} />
+      )}
     </Layout>
   )
-  // return  'this is '
 }
+
+export const query = graphql`
+    query allContentfulPost {
+      allContentfulPost {
+        edges {
+          node {
+            title
+            image {
+              title
+              file {
+                url
+              }
+            }
+            description {
+              description
+            }
+            slug
+            updatedAt(locale: "ja-JP", formatString: "YYYY年MM月DD日")
+          }
+        }
+      }
+    }
+`
